2802106868505174b8400b8c4eb8367adbce002e
David Hadley <davehadley@users.noreply.github.com>
2020-11-19 02:00:44 +0000

    Update recipe file.

--- recipes/root/all/conanfile.py
+++ recipes/root/all/conanfile.py
@@ -13,7 +13,16 @@ class RootConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = {"shared": False}
     generators = "cmake"
-    requires = "lz4/1.9.2", "xorg/system"
+    requires = (
+        "lz4/1.9.2",
+        "opengl/system",
+        "libxml2/2.9.10",
+        "glu/system",
+        "xorg/system",
+        "openssl/1.1.1h",
+        "libjpeg/9d",
+        "libpng/1.6.37",
+    )
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -25,12 +34,33 @@ class RootConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.h*", dst="include", src="include", keep_path=True)
+        self.copy("*.lib", dst="lib", src="lib", keep_path=False)
+        self.copy("*", dst="bin", src="bin", keep_path=False)
+        self.copy("*.so", dst="lib", src="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", src="lib", keep_path=False)
+        self.copy("*.a", dst="lib", src="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["root"]
+        # get this list with root-config --libs
+        self.cpp_info.libs = [
+            "Core",
+            "Imt",
+            "RIO",
+            "Net",
+            "Hist",
+            "Graf",
+            "Graf3d",
+            "Gpad",
+            "ROOTVecOps",
+            "Tree",
+            "TreePlayer",
+            "Rint",
+            "Postscript",
+            "Matrix",
+            "Physics",
+            "MathCore",
+            "Thread",
+            "MultiProc",
+            "ROOTDataFrame",
+        ]
