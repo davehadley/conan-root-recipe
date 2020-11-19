efe24a31a0c808ae74c329365b5bc36ab0d7e67a
David Hadley <davehadley@users.noreply.github.com>
2020-11-19 05:42:16 +0000

    Working on recipe.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mkdir root-build
-cd root-build && conan install ../root 2>&1 && cmake ../root && cmake --build . 
+cd root-build && conan install ../root 2>&1 && cmake -Dxrootd=OFF ../root && cmake --build . --parallel 4
 
 
--- recipes/root/all/conanfile.py
+++ recipes/root/all/conanfile.py
@@ -12,35 +12,92 @@ class RootConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     # options = {"shared": [True, False]}
     # default_options = {"shared": False}
-    generators = "cmake"
+    generators = "txt"
     requires = (
-        "lz4/1.9.2",
         "opengl/system",
         "libxml2/2.9.10",
         "glu/system",
         "xorg/system",
-        "openssl/1.1.1h",
-        "libjpeg/9d",
-        "libpng/1.6.37",
+        # "openssl/1.1.1h",
+        # "libjpeg/9d",
+        # "libpng/1.6.37",
+        "sqlite3/3.33.0",
     )
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
 
     def build(self):
+        # Paths and libraries, all
+        print("-------- ALL --------------")
+        print(self.deps_cpp_info.include_paths)
+        print(self.deps_cpp_info.lib_paths)
+        print(self.deps_cpp_info.bin_paths)
+        print(self.deps_cpp_info.libs)
+        print(self.deps_cpp_info.defines)
+        print(self.deps_cpp_info.cflags)
+        print(self.deps_cpp_info.cxxflags)
+        print(self.deps_cpp_info.sharedlinkflags)
+        print(self.deps_cpp_info.exelinkflags)
+        print("DONE!")
         cmake = CMake(self)
         version = self.version.replace("v", "")
-        cmake.configure(source_folder=f"root-{version}")
+        cmake.configure(
+            source_folder=f"root-{version}",
+            defs={
+                "fail-on-missing": "ON",
+                "CMAKE_CXX_STANDARD": str(11),
+                # prefer builtins where available
+                "builtin_pcre": "ON",
+                "builtin_lzma": "ON",
+                "builtin_zstd": "ON",
+                "builtin_xxhash": "ON",
+                "builtin_lz4": "ON",
+                "builtin_afterimage": "ON",
+                "builtin_gsl": "ON",
+                "builtin_glew": "ON",
+                "builtin_gl2ps": "ON",
+                "builtin_openssl": "ON",
+                "builtin_fftw3": "ON",
+                "builtin_cfitsio": "ON",
+                "builtin_xrootd": "ON",
+                "builtin_ftgl": "ON",
+                "builtin_davix": "ON",
+                "builtin_tbb": "ON",
+                "builtin_vdt": "ON",
+                # "builtin_asimage" : "OFF",
+                # No Conan packages available for these dependencies yet
+                "pythia6": "OFF",
+                "pythia8": "OFF",
+                "mysql": "OFF",
+                "oracle": "OFF",
+                "pgsql": "OFF",
+                "gfal": "OFF",
+                "tmva-pymva": "OFF",
+                # set paths to Conan provided depedencies
+                "LIBXML2_LIBRARY": str(self.deps_cpp_info["libxml2"].lib_paths[0]),
+                "LIBXML2_INCLUDE_DIR": str(
+                    self.deps_cpp_info["libxml2"].include_paths[0]
+                ),
+                # "CMAKE_LIBRARY_PATH" : ";".join(self.deps_cpp_info.libs),
+                # "CMAKE_INCLUDE_PATH" : ",".join(self.deps_cpp_info.include_paths),
+                "SQLITE_INCLUDE_DIR": str(
+                    self.deps_cpp_info["sqlite3"].include_paths[0]
+                ),
+                "SQLITE_LIBRARIES": str(self.deps_cpp_info["sqlite3"].lib_paths[0]),
+            },
+        )
         cmake.build()
+        cmake.install()
         cmake.test()
 
-    def package(self):
-        self.copy("*.h*", dst="include", src="include", keep_path=True)
-        self.copy("*.lib", dst="lib", src="lib", keep_path=False)
-        self.copy("*", dst="bin", src="bin", keep_path=False)
-        self.copy("*.so", dst="lib", src="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", src="lib", keep_path=False)
-        self.copy("*.a", dst="lib", src="lib", keep_path=False)
+    # def package(self):
+    #     self.copy("*.h*", dst="include", src="include", keep_path=True)
+    #     self.copy("*.lib", dst="lib", src="lib", keep_path=False)
+    #     self.copy("*", dst="bin", src="bin", keep_path=False)
+    #     self.copy("*.so", dst="lib", src="lib", keep_path=False)
+    #     self.copy("*.dylib", dst="lib", src="lib", keep_path=False)
+    #     self.copy("*.a", dst="lib", src="lib", keep_path=False)
 
     def package_info(self):
         # get this list with root-config --libs
