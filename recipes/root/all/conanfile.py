34b660d397b65df2240c71a3f9c2a9fb6d88fde4
David Hadley <davehadley@users.noreply.github.com>
2020-11-20 01:12:59 +0000

    Slowly making progress.

--- recipes/root/all/conanfile.py
+++ recipes/root/all/conanfile.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from conans import CMake, ConanFile, tools
 
 
@@ -12,32 +10,32 @@ class RootConan(ConanFile):
     description = "CERN ROOT data analysis framework."
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
-    # options = {"shared": [True, False]}
-    # default_options = {"shared": False}
+    options = {"shared": [True]}
+    default_options = {"shared": True, "libxml2:shared": True, "sqlite3:shared": True}
     generators = "txt", "cmake"
     requires = (
         "opengl/system",
         "libxml2/2.9.10",
         "glu/system",
         "xorg/system",
-        # "openssl/1.1.1h",
+        "sqlite3/3.33.0",
+        # ROOT docs claims that these are required but builds without them...
         # "libjpeg/9d",
         # "libpng/1.6.37",
-        "sqlite3/3.33.0",
     )
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], keep_permissions=True)
 
     def build(self):
-        cmake = CMake(self)
+        cmake = CMake(self, set_cmake_flags=True)
         version = self.version.replace("v", "")
         cmake.configure(
             source_folder=f"root-{version}",
             defs={
                 "fail-on-missing": "ON",
                 "CMAKE_CXX_STANDARD": str(11),
-                # prefer builtins where available
+                # Prefer builtins where available
                 "builtin_pcre": "ON",
                 "builtin_lzma": "ON",
                 "builtin_zstd": "ON",
@@ -54,8 +52,7 @@ class RootConan(ConanFile):
                 "builtin_davix": "ON",
                 "builtin_tbb": "ON",
                 "builtin_vdt": "ON",
-                # "builtin_asimage" : "OFF",
-                # Doesn't build with builtin openssl.
+                # xrootd doesn't build with builtin openssl.
                 "builtin_xrootd": "OFF",
                 "xrootd": "OFF",
                 # No Conan packages available for these dependencies yet
@@ -66,13 +63,13 @@ class RootConan(ConanFile):
                 "pgsql": "OFF",
                 "gfal": "OFF",
                 "tmva-pymva": "OFF",
-                # set paths to Conan provided depedencies
-                # "LIBXML2_LIBRARY": self._getlibsopt("libxml2"),
-                # "LIBXML2_INCLUDE_DIR": self._getincludeopt("libxml2"),
+                # Tell CMake where to look for Conan provided depedencies
                 "CMAKE_LIBRARY_PATH": ";".join(self.deps_cpp_info.lib_paths),
                 "CMAKE_INCLUDE_PATH": ";".join(self.deps_cpp_info.include_paths),
-                # "SQLITE_INCLUDE_DIR": self._getincludeopt("sqlite3"),
-                # "SQLITE_LIBRARIES": self._getlibsopt("sqlite3"),
+                # Sqlite needs some special treatment
+                "SQLITE_INCLUDE_DIR": self._getincludeopt("sqlite3"),
+                "SQLITE_LIBRARIES": self._getalllibs("sqlite3"),
+                "CMAKE_VERBOSE_MAKEFILE": "ON",
             },
         )
         cmake.build()
@@ -87,19 +84,24 @@ class RootConan(ConanFile):
     #     self.copy("*.dylib", dst="lib", src="lib", keep_path=False)
     #     self.copy("*.a", dst="lib", src="lib", keep_path=False)
 
+    def _getalllibs(self, dep: str) -> str:
+        return ";".join(
+            self.deps_cpp_info[dep].libs + self.deps_cpp_info[dep].system_libs
+        )
+
     def _getincludeopt(self, depname: str) -> str:
         return ";".join(self.deps_cpp_info[depname].include_paths)
 
-    def _getlibsopt(self, depname: str) -> str:
-        paths = self.deps_cpp_info[depname].lib_paths
-        libpaths = [
-            str(lib)
-            for p in paths
-            for pattern in ("*.so", "*.dll", "*.dylib", "*.lib", "*.a")
-            for lib in Path(p).glob(pattern)
-        ]
-        assert len(libpaths) >= 1
-        return ";".join(libpaths)
+    # def _getlibsopt(self, depname: str) -> str:
+    #     paths = self.deps_cpp_info[depname].lib_paths
+    #     libpaths = [
+    #         str(lib)
+    #         for p in paths
+    #         for pattern in ("*.so", "*.dll", "*.dylib", "*.lib", "*.a")
+    #         for lib in Path(p).glob(pattern)
+    #     ]
+    #     assert len(libpaths) >= 1
+    #     return ";".join(libpaths)
 
     def package_info(self):
         # get this list with root-config --libs
