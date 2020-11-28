e1a3a71037b48504472284e88a5015e88127c17b
David Hadley <davehadley@users.noreply.github.com>
2020-11-28 11:52:44 +0000

    Update setup.sh to use venv.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
 
-conda activate conan-root-recipe
+if [ ! -d "venv" ]
+then
+    python3 -m venv venv \
+    && source venv/bin/activate \
+    && python -m pip install conan;
+fi
+
+source venv/bin/activate
