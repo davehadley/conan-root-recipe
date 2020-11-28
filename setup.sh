b530efcfb11baf6d4dda521f08d326639375032d
David Hadley <davehadley@users.noreply.github.com>
2020-11-28 11:54:31 +0000

    Revert "Update setup.sh to use venv."
    This reverts commit e1a3a71037b48504472284e88a5015e88127c17b.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,2 @@
 
-if [ ! -d "venv" ]
-then
-    python3 -m venv venv \
-    && source venv/bin/activate \
-    && python -m pip install conan;
-fi
-
-source venv/bin/activate
+conda activate conan-root-recipe
