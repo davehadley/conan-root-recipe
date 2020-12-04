5bfc21a91e89b70fdb0ca667251bf385a7b03e2a
David Hadley <davehadley@users.noreply.github.com>
2020-12-04 14:05:20 +0000

    Reducing test size.

--- recipes/root/all/test_package/conanfile.py
+++ recipes/root/all/test_package/conanfile.py
@@ -6,16 +6,16 @@ from conans import CMake, ConanFile, RunEnvironment, tools
 class RootTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = ("cmake_paths", "cmake_find_package")
-    requires = ("catch2/2.13.3",)
 
     def build(self):
         env_build = RunEnvironment(self)
         with tools.environment_append(env_build.vars):
             cmake = CMake(self)
-            cmake.configure(defs={"CMAKE_VERBOSE_MAKEFILE": "ON"})
+            cmake.configure(
+                defs={"CMAKE_VERBOSE_MAKEFILE": "ON", "CMAKE_BUILD_TYPE": "Debug"}
+            )
             cmake.build()
 
     def test(self):
         if not tools.cross_building(self):
-            # self.run("ctest .", run_environment=True)
-            self.run(f".{os.sep}example", run_environment=True)
+            self.run(".{}testrootio".format(os.sep), run_environment=True)
--- recipes/root/all/test_package/testrootio.cpp
+++ recipes/root/all/test_package/testrootio.cpp
@@ -1,6 +1,4 @@
-#define CATCH_CONFIG_MAIN 
-#include <catch2/catch.hpp>
-
+#include <assert.h>
 #include <iostream>
 #include "TRandom3.h"
 #include "TH1F.h"
@@ -10,22 +8,11 @@
 #include "TTreeReader.h"
 #include "Event.hpp"
 
-TEST_CASE( "Basic histogram operation", "[hist]" ) {
-    int N = 100;
-    auto hist = new TH1F (
-        "testhist",
-        "This is a test",
-        100,
-        -5.0,
-        5.0
-    );
-    TRandom3 random(1234);
-    for (int i=0; i<N; ++i) {
-        hist->Fill(random.Gaus(0.0, 1.0));
-    }
-    int actual = hist->GetEntries();
-    REQUIRE(N == actual);
+
+void check(bool result, std::string message) {
+    if (!result) { throw std::runtime_error("testrootio FAILED : " + message); }
 }
+
 
 void create_events_file(std::string name = "testevents.root", const int Nevent = 10, const int Npart = 10) {
     auto tfile = TFile::Open(name.c_str(), "RECREATE");
@@ -46,7 +33,7 @@ void create_events_file(std::string name = "testevents.root", const int Nevent =
     delete tfile;
 }
 
-TEST_CASE( "Read/Write Events to File", "[tree]" ) {
+int main() {
     gSystem->Load("libEvent");
     const std::string fname = "testevents.root";
     const int Nevent = 10;
@@ -56,20 +43,18 @@ TEST_CASE( "Read/Write Events to File", "[tree]" ) {
     auto tree = tfile->Get<TTree>("tree");
     Event* event = 0;
     tree->SetBranchAddress("events", &event);
-    REQUIRE(tree->GetEntries() == Nevent);
+    check (tree->GetEntries() == Nevent, "event count mismatch");
     for(int eventnum = 0; eventnum < Nevent; ++eventnum) {
-        REQUIRE(tree->GetEntry(eventnum)>0);
+        check(tree->GetEntry(eventnum)>0, "read zero event size");
         auto ev = event;    
-        REQUIRE(ev->particles.size() == Npart);
+        check(ev->particles.size() == Npart, "read no particles");
         for(int index = 0; index < Npart; ++index) {
             auto& p = ev->particles.at(index);
-            REQUIRE(p.getID() == index);
-            REQUIRE(p.getP4().X() == 1.0);
-            REQUIRE(p.getP4().Y() == 2.0);
-            REQUIRE(p.getP4().Z() == 3.0);
-            REQUIRE(p.getP4().T() == 4.0);
+            check(p.getID() == index, "read bad index");
+            check(p.getP4().X() == 1.0, "read bad 4-vector");
         }
     }
+    return 0;
 }
 
 
