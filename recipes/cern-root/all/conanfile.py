e4863b838fb84d7cb580e54b3074f249393cabab
David Hadley <davehadley@users.noreply.github.com>
2021-01-23 17:23:40 +0000

    Change version numbering system to match ROOT official version numbers https://root.cern/about/versioning/.

--- recipes/cern-root/all/conanfile.py
+++ recipes/cern-root/all/conanfile.py
@@ -21,7 +21,9 @@ required_conan_version = ">=1.29.1"
 
 class CernRootConan(ConanFile):
     name = "cern-root"
-    version = "v6-22-02"
+    # v in version is intentional, ROOT does not follow strict SemVer.
+    # see: https://root.cern/about/versioning/
+    version = "v6.22.06"
     license = "LGPL-2.1-or-later"  # of ROOT itself, the recipe is under MIT license.
     homepage = "https://root.cern/"
     # ROOT itself is located at: https://github.com/root-project/root
@@ -113,7 +115,7 @@ class CernRootConan(ConanFile):
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename(
-            "root-{}".format(self.version.replace("v", "")),
+            "root-{}".format(self.version.replace("v", "").replace(".", "-")),
             self._source_subfolder,
         )
 
